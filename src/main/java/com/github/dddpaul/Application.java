84576d70d026aad1710bc5eed346920279b6e904
Pavel Derendyaev <paul@reic.ru>
2015-11-15 16:59:13 +0300

    Use @Controller annotation instead @Bean

--- src/main/java/com/github/dddpaul/Application.java
+++ src/main/java/com/github/dddpaul/Application.java
@@ -1,10 +1,12 @@
 package com.github.dddpaul;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,48 +30,34 @@ public class Application {
     @Configuration
     @EnableAutoConfiguration
     static class ServiceOneConfiguration {
-        @Bean
-        public ControllerOne controller(SharedService service) {
-            return new ControllerOne(service);
+        @Controller
+        @RequestMapping("/one")
+        static class ControllerOne {
+            @Autowired
+            private SharedService service;
+
+            @RequestMapping
+            @ResponseBody
+            public String getMessage() {
+                return "ControllerOne says \"" + service.getMessage() + "\"";
+            }
         }
     }
 
     @Configuration
     @EnableAutoConfiguration
     static class ServiceTwoConfiguration {
-        @Bean
-        public ControllerTwo controller(SharedService service) {
-            return new ControllerTwo(service);
-        }
-    }
+        @Controller
+        @RequestMapping("/two")
+        static class ControllerOne {
+            @Autowired
+            private SharedService service;
 
-    @RequestMapping("/one")
-    static class ControllerOne {
-        private SharedService service;
-
-        public ControllerOne(SharedService service) {
-            this.service = service;
-        }
-
-        @RequestMapping
-        @ResponseBody
-        public String getMessage() {
-            return "ControllerOne says \"" + this.service.getMessage() + "\"";
-        }
-    }
-
-    @RequestMapping("/two")
-    static class ControllerTwo {
-        private SharedService service;
-
-        public ControllerTwo(SharedService service) {
-            this.service = service;
-        }
-
-        @RequestMapping
-        @ResponseBody
-        public String getMessage() {
-            return "ControllerTwo says \"" + this.service.getMessage() + "\"";
+            @RequestMapping
+            @ResponseBody
+            public String getMessage() {
+                return "ControllerTwo says \"" + service.getMessage() + "\"";
+            }
         }
     }
 }
